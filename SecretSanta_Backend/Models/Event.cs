3da14e2b7f65811b0e51c7ef72e004315e4cd0ef
Keklil <world20866@gmail.com>
2022-04-04 16:11:48 +0500

    Fix db connection string

--- SecretSanta_Backend/Models/Event.cs
+++ SecretSanta_Backend/Models/Event.cs
@@ -7,7 +7,7 @@ namespace SecretSanta_Backend.Models
     {
         public Guid Id { get; set; }
         public string? Description { get; set; }
-        public DateOnly? Endofregistration { get; set; }
-        public DateOnly? Endofevent { get; set; }
+        public DateTime? Endofregistration { get; set; }
+        public DateTime? Endofevent { get; set; }
     }
 }
