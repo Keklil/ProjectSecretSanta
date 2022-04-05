4d08f6701959300ec6100c5fc76ad1d944b11f00
Keklil <world20866@gmail.com>
2022-04-05 19:27:35 +0500

    Add delete event api, add Data Transfer Objects

---
+++ SecretSanta_Backend/ModelsDTO/MemberEventDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SecretSanta_Backend.ModelsDTO
+{
+    public partial class MemberEventDTO
+    {
+        public Guid MemberId { get; set; }
+        public Guid EventId { get; set; }
+        public bool? MemberAttend { get; set; }
+        public string? DeliveryService { get; set; }
+        public string? TrackNumber { get; set; }
+        public string? Preference { get; set; }
+        public Guid? Recipient { get; set; }
+        public DateTime? Sendday { get; set; }
+
+        public virtual EventDTO Event { get; set; } = null!;
+        public virtual MemberDTO Member { get; set; } = null!;
+    }
+}
