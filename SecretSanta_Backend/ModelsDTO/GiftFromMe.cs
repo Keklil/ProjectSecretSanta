34bda49fdaa4fc1a0f71c80c3270d2617a43f484
Keklil <world20866@gmail.com>
2022-05-07 14:31:02 +0500

    Fineshed member API, add create event dto for admin API.

---
+++ SecretSanta_Backend/ModelsDTO/GiftFromMe.cs
@@ -0,0 +1,8 @@
+﻿namespace SecretSanta_Backend.ModelsDTO
+{
+    public class GiftFromMe
+    {
+        public string Preferences { get; set; }
+        public string Address { get; set; }
+    }
+}
